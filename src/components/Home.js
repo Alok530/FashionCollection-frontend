52cbb8099cb2d35675900fcda2da67651ea4c5f2
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2022-12-13 19:14:53 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Footer from './Footer'
+import ScrollBtn from './ScrollBtn';
+import { Link } from 'react-router-dom';
+import Navbarfront from './Navbarfront'
+
+const Home = () => {
+    return (
+        <>
+            <ScrollBtn />
+            <Navbarfront/>
+            <div className="contact Home">
+                <div className="content">
+                    <h1>Fashion Collections</h1>
+                    <h3>FASHION IS WHAT YOU BUY</h3>
+                    <h5> Style is what you do with it</h5>
+                </div>
+                <Link className="temp mx-2" style={{ textDecoration: 'none', textAlign: 'center' }} to="/login" role="button">Shop Now</Link>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
